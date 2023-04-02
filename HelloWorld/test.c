73548d1834a7556c51c51cadd61f4853f3dd8a4a
YibufanFay <yibufanfay@outlook.com>
2023-04-02 16:48:56 +0800

    Hello World!!

---
+++ HelloWorld/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("   Hello World!   \n");
+	printf("    Hello Bite    \n");
+	printf(" 在接下来的日子里  \n");
+	printf("     请多多指教    \n");
+	printf("     望学有所成    \n");
+	return 0;
+}
